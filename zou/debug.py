2a20ab701c918dccf4e20b326c407c66a3ff98a2
Frank Rousseau <frank.rousseau@free.fr>
2020-09-01 11:09:18 +0200

    [dev] Add loggging to new dev server runner

--- zou/debug.py
+++ zou/debug.py
@@ -1,6 +1,9 @@
+import logging
 from flask_socketio import SocketIO
 from zou.app import app
 
+FORMAT = "%(message)s"
+logging.basicConfig(level=logging.INFO, format=FORMAT)
 socketio = SocketIO(app)
 
 if __name__ == "__main__":
