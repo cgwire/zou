4a5e6373692798eb4d48c294d3262c93902b27de
Frank Rousseau <frank.rousseau@free.fr>
2021-01-13 12:32:42 +0100

    [schedule] Allow schedule edition by supervisors

--- zou/app/blueprints/crud/schedule_item.py
+++ zou/app/blueprints/crud/schedule_item.py
@@ -2,6 +2,8 @@ from zou.app.models.schedule_item import ScheduleItem
 
 from .base import BaseModelResource, BaseModelsResource
 
+from zou.app.services import user_service
+
 
 class ScheduleItemsResource(BaseModelsResource):
     def __init__(self):
@@ -11,6 +13,9 @@ class ScheduleItemsResource(BaseModelsResource):
 class ScheduleItemResource(BaseModelResource):
     def __init__(self):
         BaseModelResource.__init__(self, ScheduleItem)
+
+    def check_update_permissions(self, instance, data):
+        return user_service.check_manager_project_access(instance["project_id"])
 
     def update_data(self, data, instance_id):
         if isinstance(data.get("man_days", None), str):
