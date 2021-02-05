a70416d187f4876e5a82b18377e796b0d8b885fd
Pierre-Louis Bonicoli <pierre-louis.bonicoli@libregerbil.fr>
2021-02-05 14:06:38 +0100

    Remove unused parameter 'playlist'
    Complete 4bee9280acf351d21d06c568813ed755fad97f79

--- zou/app/services/playlists_service.py
+++ zou/app/services/playlists_service.py
@@ -420,7 +420,7 @@ def build_playlist_movie_file(playlist, app=None):
     job = start_build_job(playlist)
     project = projects_service.get_project(playlist["project_id"])
     tmp_file_paths = retrieve_playlist_tmp_files(playlist, only_movies=True)
-    movie_file_path = get_playlist_movie_file_path(playlist, job)
+    movie_file_path = get_playlist_movie_file_path(job)
     (width, height) = preview_files_service.get_preview_file_dimensions(project)
     fps = preview_files_service.get_preview_file_fps(project)
 
@@ -529,7 +529,7 @@ def get_playlist_file_name(playlist):
     return slugify(attachment_filename)
 
 
-def get_playlist_movie_file_path(playlist, build_job):
+def get_playlist_movie_file_path(build_job):
     """
     Build file path for the movie file matching given playlist.
     """
@@ -585,7 +585,7 @@ def remove_build_job(playlist, build_job_id):
     hard drive.
     """
     job = BuildJob.get(build_job_id)
-    movie_file_path = get_playlist_movie_file_path(playlist, job.serialize())
+    movie_file_path = get_playlist_movie_file_path(job.serialize())
     if os.path.exists(movie_file_path):
         os.remove(movie_file_path)
     try:
