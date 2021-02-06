efbcd8104470234e50ad2e40719b0edf1fbc45c4
Frank Rousseau <frank.rousseau@free.fr>
2021-02-06 16:05:20 +0100

    [utils] Add helper to handle timezone in date strings

--- zou/app/utils/date_helpers.py
+++ zou/app/utils/date_helpers.py
@@ -1,4 +1,5 @@
-from datetime import date, timedelta
+from babel.dates import format_datetime
+from datetime import date, datetime, timedelta
 
 
 def get_date_from_now(nb_days):
@@ -7,3 +8,15 @@ def get_date_from_now(nb_days):
 
 def get_date_diff(date_a, date_b):
     return abs((date_b - date_a).total_seconds())
+
+
+def get_date_string_with_timezone(date_string, timezone):
+    """
+    Apply given timezone to given date and return it as a string.
+    """
+    date_obj = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S")
+    return format_datetime(
+        date_obj,
+        "YYYY-MM-DDTHH:mm:ss",
+        tzinfo=timezone
+    )
