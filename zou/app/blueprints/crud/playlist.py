3db864da56b8dda1cf6383ac744eb8229e615c02
Frank Rousseau <frank.rousseau@free.fr>
2021-05-17 21:45:31 +0200

    [playlists] Fix playlist creation when task type is not properly given

--- zou/app/blueprints/crud/playlist.py
+++ zou/app/blueprints/crud/playlist.py
@@ -2,6 +2,7 @@ from zou.app.models.playlist import Playlist
 from zou.app.services import user_service, playlists_service
 
 from .base import BaseModelResource, BaseModelsResource
+from zou.app.utils import fields
 
 
 class PlaylistsResource(BaseModelsResource):
@@ -17,6 +18,9 @@ class PlaylistsResource(BaseModelsResource):
     def update_data(self, data):
         if "episode_id" in data and data["episode_id"] in ["all", "main"]:
             data["episode_id"] = None
+        if "task_type_id" in data and \
+           not fields.is_valid_id(data["task_type_id"]):
+            data["task_type_id"] = None
         return data
 
 
