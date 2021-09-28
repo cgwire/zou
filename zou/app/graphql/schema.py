6442a2617cae3827110eb87454f70cfc7575b4d9
Simon Lambin <slambin@artfx.fr>
2021-09-28 14:41:17 +0200

    Create converter for sqlalchemy types

--- zou/app/graphql/schema.py
+++ zou/app/graphql/schema.py
@@ -1,8 +1,35 @@
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
+from sqlalchemy.orm import aliased
+from sqlalchemy_utils import UUIDType, EmailType, LocaleType, TimezoneType
+from graphene_sqlalchemy.converter import convert_sqlalchemy_type
+
 from zou.app.models.person import Person as PersonModel
 from zou.app.models.comment import Comment as CommentModel
+from zou.app.models.project import Project as ProjectModel
+
+from zou.app import db
+
+@convert_sqlalchemy_type.register(UUIDType)
+def convert_uuid(type, column, registry=None):
+    return graphene.ID
+
+@convert_sqlalchemy_type.register(EmailType)
+def convert_email(type, column, registry=None):
+    return graphene.String
+
+@convert_sqlalchemy_type.register(LocaleType)
+def convert_locale(type, column, registry=None):
+    return graphene.String
+
+@convert_sqlalchemy_type.register(TimezoneType)
+def convert_timezone(type, column, registry=None):
+    return graphene.String
+
+@convert_sqlalchemy_type.register(db.LargeBinary)
+def convert_timezone(type, column, registry=None):
+    return graphene.String
 
 class Person(SQLAlchemyObjectType):
     class Meta:
@@ -14,11 +41,16 @@ class Comment(SQLAlchemyObjectType):
         model = CommentModel
         interfaces = (relay.Node, )
 
+class Project(SQLAlchemyObjectType):
+    class Meta:
+        model = ProjectModel
+        interfaces = (relay.Node, )
+
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
     # Allows sorting over multiple columns, by default over the primary key
     all_comments = SQLAlchemyConnectionField(Comment.connection)
-    # Allows sorting over multiple columns, by default over the primary key
     all_persons = SQLAlchemyConnectionField(Person.connection)
+    all_projects = SQLAlchemyConnectionField(Project.connection)
 
 schema = graphene.Schema(query=Query)
