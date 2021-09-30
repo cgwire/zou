2bdd5c5d4e9cf6b6d69b8003fb3d521b622422df
Simon Lambin <slambin@artfx.fr>
2021-09-30 15:55:51 +0200

    Add lambda to the custom fields

--- zou/app/graphql/schema.py
+++ zou/app/graphql/schema.py
@@ -50,8 +50,8 @@ class Task(SQLAlchemyObjectType):
         model = TaskModel
 
     previews = graphene.List(PreviewFile, resolver=DefaultResolver(PreviewFile, PreviewFileModel, "task_id"))
-    status = graphene.Field(TaskStatus, resolver=DefaultResolver(TaskStatus, TaskStatusModel, "id", "task_status_id"))
-    type = graphene.Field(TaskType, resolver=DefaultResolver(TaskStatus, TaskStatusModel, "id", "task_type_id"))
+    status = graphene.Field(lambda: TaskStatus, resolver=DefaultResolver(TaskStatus, TaskStatusModel, "id", "task_status_id"))
+    type = graphene.Field(lambda: TaskType, resolver=DefaultResolver(TaskStatus, TaskStatusModel, "id", "task_type_id"))
 
 class EntityType(SQLAlchemyObjectType):
     class Meta:
@@ -102,7 +102,7 @@ class Comment(SQLAlchemyObjectType):
     class Meta:
         model = CommentModel
 
-    person = graphene.Field(Person, resolver=DefaultResolver(Person, TaskStatusModel, "id", "person_id"))
+    person = graphene.Field(lambda: Person, resolver=DefaultResolver(Person, TaskStatusModel, "id", "person_id"))
 
 class Query(graphene.ObjectType):
     softwares = graphene.List(Software, resolver=DefaultResolver(Software))
