779b728b6fdaa8e87c0213846df85323034b70b3
Simon Lambin <slambin@artfx.fr>
2021-10-04 13:51:55 +0200

    test dynamic types

--- zou/app/graphql/resolvers.py
+++ zou/app/graphql/resolvers.py
@@ -4,9 +4,15 @@ from zou.app.services import (
     entities_service,
 )
 
-class DefaultResolver():
 
-    def __init__(self, model_type: db.Model = None, foreign_key: str = "", parent_key: str = "id", query_all: bool = True):
+class DefaultResolver:
+    def __init__(
+        self,
+        model_type: db.Model = None,
+        foreign_key: str = "",
+        parent_key: str = "id",
+        query_all: bool = True,
+    ):
         self.model_type = model_type
         self.foreign_key = foreign_key
         self.parent_key = parent_key
@@ -26,25 +32,32 @@ class DefaultResolver():
     def get_query(self, root):
         query = self.model_type.query
         if all([root, self.model_type, self.foreign_key]):
-            query = query.filter(getattr(self.model_type, self.foreign_key) == getattr(root, self.parent_key))
+            query = query.filter(
+                getattr(self.model_type, self.foreign_key)
+                == getattr(root, self.parent_key)
+            )
         return query
 
     def __call__(self, root, info, **kwargs):
         query = self.get_query(root)
 
-        print(kwargs)
         for filter_set in kwargs.get("filters", []):
             for filter in filter_set.get("filters", []):
-                query = query.filter(self.add_filter(getattr(self.model_type, filter_set["field"]), filter["filter_value"], filter["filter_type"]))
+                query = query.filter(
+                    self.add_filter(
+                        getattr(self.model_type, filter_set["field"]),
+                        filter["filter_value"],
+                        filter["filter_type"],
+                    )
+                )
 
         if self.query_all:
             return query.all()
         else:
             return query.first()
-        
+
 
 class EntityResolver(DefaultResolver):
-
     def __init__(self, entity_type: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.entity_type_name = entity_type
@@ -53,7 +66,9 @@ class EntityResolver(DefaultResolver):
 
     @property
     def entity_type(self):
-        entity_type = entities_service.get_entity_type_by_name(self.entity_type_name)
+        entity_type = entities_service.get_entity_type_by_name(
+            self.entity_type_name
+        )
 
         if entity_type is None:
             raise KeyError("Invalid entity type name")
@@ -61,20 +76,22 @@ class EntityResolver(DefaultResolver):
 
     def get_query(self, root):
         query = super().get_query(root)
-        query = query.filter(self.model_type.entity_type_id == self.entity_type["id"])
+        query = query.filter(
+            self.model_type.entity_type_id == self.entity_type["id"]
+        )
         return query
 
-class EntityChildResolver(EntityResolver):
 
+class EntityChildResolver(EntityResolver):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.foreign_key = "parent_id"
 
-class PreviewUrlResolver(DefaultResolver):
 
+class PreviewUrlResolver(DefaultResolver):
     def __init__(self, lod: str):
         self.lod = lod
-    
+
     def __call__(self, root, info, **kwargs):
         if root is None:
             return ""
--- zou/app/graphql/schema.py
+++ zou/app/graphql/schema.py
@@ -13,121 +13,256 @@ from zou.app.models.entity_type import EntityType as EntityTypeModel
 from zou.app.models.entity import Entity as EntityModel
 from zou.app.models.project_status import ProjectStatus as ProjectStatusModel
 from zou.app.models.project import Project as ProjectModel
-from zou.app.models.attachment_file import AttachmentFile as AttachmentFileModel
+from zou.app.models.attachment_file import (
+    AttachmentFile as AttachmentFileModel,
+)
 from zou.app.models.comment import Comment as CommentModel
 from zou.app.models.department import Department as DepartmentModel
 from zou.app.models.person import Person as PersonModel
-from zou.app.graphql.resolvers import DefaultResolver, EntityResolver, EntityChildResolver, PreviewUrlResolver
-from zou.app.graphql.filter import FilterSet
+from zou.app.graphql.resolvers import (
+    DefaultResolver,
+    EntityResolver,
+    EntityChildResolver,
+    PreviewUrlResolver,
+)
+from zou.app.graphql.filter import FilterSet, test_dynamic
 from zou.app.graphql import converters
+
 
 class Software(SQLAlchemyObjectType):
     class Meta:
         model = SoftwareModel
 
+
 class WorkingFile(SQLAlchemyObjectType):
     class Meta:
         model = WorkingFileModel
+
 
 class OutputType(SQLAlchemyObjectType):
     class Meta:
         model = OutputTypeModel
 
+
 class OutputFile(SQLAlchemyObjectType):
     class Meta:
         model = OutputFileModel
 
+
 class PreviewFile(SQLAlchemyObjectType):
     class Meta:
         model = PreviewFileModel
-        
-    file_url = graphene.Field(graphene.String, resolver=PreviewUrlResolver(lod="originals"), lod=graphene.String(required=False))
+
+    file_url = graphene.Field(
+        graphene.String,
+        resolver=PreviewUrlResolver(lod="originals"),
+        lod=graphene.String(required=False),
+    )
+
 
 class Comment(SQLAlchemyObjectType):
     class Meta:
         model = CommentModel
 
-    person = graphene.Field("zou.app.graphql.schema.Person", resolver=DefaultResolver(PersonModel, "id", "person_id"), filters=graphene.List(FilterSet, required=False))
+    person = graphene.Field(
+        "zou.app.graphql.schema.Person",
+        resolver=DefaultResolver(PersonModel, "id", "person_id"),
+        filters=graphene.List(FilterSet, required=False),
+    )
+
 
 class TaskType(SQLAlchemyObjectType):
     class Meta:
         model = TaskTypeModel
 
+
 class TaskStatus(SQLAlchemyObjectType):
     class Meta:
         model = TaskStatusModel
+
 
 class Task(SQLAlchemyObjectType):
     class Meta:
         model = TaskModel
 
-    previews = graphene.List(PreviewFile, resolver=DefaultResolver(PreviewFileModel, "task_id"), filters=graphene.List(FilterSet, required=False))
-    status = graphene.Field(TaskStatus, resolver=DefaultResolver(TaskStatusModel, "id", "task_status_id"), filters=graphene.List(FilterSet, required=False))
-    type = graphene.Field(TaskType, resolver=DefaultResolver(TaskStatusModel, "id", "task_type_id"), filters=graphene.List(FilterSet, required=False))
- 
+    previews = graphene.List(
+        PreviewFile,
+        resolver=DefaultResolver(PreviewFileModel, "task_id"),
+        filters=graphene.List(FilterSet, required=False),
+    )
+    status = graphene.Field(
+        TaskStatus,
+        resolver=DefaultResolver(TaskStatusModel, "id", "task_status_id"),
+        filters=graphene.List(FilterSet, required=False),
+    )
+    type = graphene.Field(
+        TaskType,
+        resolver=DefaultResolver(TaskStatusModel, "id", "task_type_id"),
+        filters=graphene.List(FilterSet, required=False),
+    )
+
+
 class EntityType(SQLAlchemyObjectType):
     class Meta:
         model = EntityTypeModel
 
+
 class Shot(SQLAlchemyObjectType):
     class Meta:
         model = EntityModel
-    
-    tasks = graphene.List(Task, resolver=DefaultResolver(TaskModel, "entity_id"), filters=graphene.List(FilterSet, required=False))
- 
+
+    tasks = graphene.List(
+        Task,
+        resolver=DefaultResolver(TaskModel, "entity_id"),
+        filters=graphene.List(FilterSet, required=False),
+    )
+
+
 class Sequence(SQLAlchemyObjectType):
     class Meta:
         model = EntityModel
 
-    shots = graphene.List(Shot, resolver=EntityChildResolver("Shot", EntityModel), filters=graphene.List(FilterSet, required=False))
- 
+    shots = graphene.List(
+        Shot,
+        resolver=EntityChildResolver("Shot", EntityModel),
+        filters=test_dynamic(EntityModel),
+    )
+
+
 class Asset(SQLAlchemyObjectType):
     class Meta:
         model = EntityModel
-    
-    tasks = graphene.List(Task, resolver=DefaultResolver(TaskModel, "entity_id"), filters=graphene.List(FilterSet, required=False))
- 
+
+    tasks = graphene.List(
+        Task,
+        resolver=DefaultResolver(TaskModel, "entity_id"),
+        filters=graphene.List(FilterSet, required=False),
+    )
+
+
 class ProjectStatus(SQLAlchemyObjectType):
     class Meta:
         model = ProjectStatusModel
+
 
 class Project(SQLAlchemyObjectType):
     class Meta:
         model = ProjectModel
 
-    sequences = graphene.List(Sequence, resolver=EntityResolver("Sequence", EntityModel), filters=graphene.List(FilterSet, required=False))
-    assets = graphene.List(Sequence, resolver=EntityResolver("Asset", EntityModel), filters=graphene.List(FilterSet, required=False))
- 
+    sequences = graphene.List(
+        Sequence,
+        resolver=EntityResolver("Sequence", EntityModel),
+        filters=graphene.List(FilterSet, required=False),
+    )
+    assets = graphene.List(
+        Sequence,
+        resolver=EntityResolver("Asset", EntityModel),
+        filters=graphene.List(FilterSet, required=False),
+    )
+
+
 class AttachmentFile(SQLAlchemyObjectType):
     class Meta:
         model = AttachmentFileModel
+
 
 class Department(SQLAlchemyObjectType):
     class Meta:
         model = DepartmentModel
 
+
 class Person(SQLAlchemyObjectType):
     class Meta:
         model = PersonModel
 
-    comments = graphene.List(Comment, resolver=DefaultResolver(CommentModel, "person_id"), filters=graphene.List(FilterSet, required=False))
+    comments = graphene.List(
+        Comment,
+        resolver=DefaultResolver(CommentModel, "person_id"),
+        filters=graphene.List(FilterSet, required=False),
+    )
+
 
 class Query(graphene.ObjectType):
-    softwares = graphene.List(Software, resolver=DefaultResolver(SoftwareModel), filters=graphene.List(FilterSet, required=False))
-    output_types = graphene.List(OutputType, resolver=DefaultResolver(OutputTypeModel), filters=graphene.List(FilterSet, required=False))
-    output_files = graphene.List(OutputFile, resolver=DefaultResolver(OutputFileModel), filters=graphene.List(FilterSet, required=False))
-    preview_files = graphene.List(PreviewFile, resolver=DefaultResolver(PreviewFileModel), filters=graphene.List(FilterSet, required=False))
-    task_types = graphene.List(TaskType, resolver=DefaultResolver(TaskTypeModel), filters=graphene.List(FilterSet, required=False))
-    task_status = graphene.List(TaskStatus, resolver=DefaultResolver(TaskStatusModel), filters=graphene.List(FilterSet, required=False))
-    tasks = graphene.List(Task, resolver=DefaultResolver(TaskModel), filters=graphene.List(FilterSet, required=False))
-    entity_types = graphene.List(EntityType, resolver=DefaultResolver(EntityTypeModel), filters=graphene.List(FilterSet, required=False))
-    shots = graphene.List(Shot, resolver=EntityResolver("Shot", EntityModel), filters=graphene.List(FilterSet, required=False))
-    sequences = graphene.List(Sequence, resolver=EntityResolver("Sequence", EntityModel), filters=graphene.List(FilterSet, required=False))
-    assets = graphene.List(Asset, resolver=EntityResolver("Asset", EntityModel), filters=graphene.List(FilterSet, required=False))
-    project_status = graphene.List(ProjectStatus, resolver=DefaultResolver(ProjectStatusModel), filters=graphene.List(FilterSet, required=False))
-    projects = graphene.List(Project, resolver=DefaultResolver(ProjectModel), filters=graphene.List(FilterSet, required=False))
-    attachment_files = graphene.List(AttachmentFile, resolver=DefaultResolver(AttachmentFileModel), filters=graphene.List(FilterSet, required=False))
-    comments = graphene.List(Comment, resolver=DefaultResolver(CommentModel), filters=graphene.List(FilterSet, required=False))
-    persons = graphene.List(Person, resolver=DefaultResolver(PersonModel), filters=graphene.List(FilterSet, required=False))
+    softwares = graphene.List(
+        Software,
+        resolver=DefaultResolver(SoftwareModel),
+        filters=graphene.List(FilterSet, required=False),
+    )
+    output_types = graphene.List(
+        OutputType,
+        resolver=DefaultResolver(OutputTypeModel),
+        filters=graphene.List(FilterSet, required=False),
+    )
+    output_files = graphene.List(
+        OutputFile,
+        resolver=DefaultResolver(OutputFileModel),
+        filters=graphene.List(FilterSet, required=False),
+    )
+    preview_files = graphene.List(
+        PreviewFile,
+        resolver=DefaultResolver(PreviewFileModel),
+        filters=graphene.List(FilterSet, required=False),
+    )
+    task_types = graphene.List(
+        TaskType,
+        resolver=DefaultResolver(TaskTypeModel),
+        filters=graphene.List(FilterSet, required=False),
+    )
+    task_status = graphene.List(
+        TaskStatus,
+        resolver=DefaultResolver(TaskStatusModel),
+        filters=graphene.List(FilterSet, required=False),
+    )
+    tasks = graphene.List(
+        Task,
+        resolver=DefaultResolver(TaskModel),
+        filters=graphene.List(FilterSet, required=False),
+    )
+    entity_types = graphene.List(
+        EntityType,
+        resolver=DefaultResolver(EntityTypeModel),
+        filters=graphene.List(FilterSet, required=False),
+    )
+    shots = graphene.List(
+        Shot,
+        resolver=EntityResolver("Shot", EntityModel),
+        filters=graphene.List(FilterSet, required=False),
+    )
+    sequences = graphene.List(
+        Sequence,
+        resolver=EntityResolver("Sequence", EntityModel),
+        filters=graphene.List(FilterSet, required=False),
+    )
+    assets = graphene.List(
+        Asset,
+        resolver=EntityResolver("Asset", EntityModel),
+        filters=graphene.List(FilterSet, required=False),
+    )
+    project_status = graphene.List(
+        ProjectStatus,
+        resolver=DefaultResolver(ProjectStatusModel),
+        filters=graphene.List(FilterSet, required=False),
+    )
+    projects = graphene.List(
+        Project,
+        resolver=DefaultResolver(ProjectModel),
+        filters=graphene.List(FilterSet, required=False),
+    )
+    attachment_files = graphene.List(
+        AttachmentFile,
+        resolver=DefaultResolver(AttachmentFileModel),
+        filters=graphene.List(FilterSet, required=False),
+    )
+    comments = graphene.List(
+        Comment,
+        resolver=DefaultResolver(CommentModel),
+        filters=graphene.List(FilterSet, required=False),
+    )
+    persons = graphene.List(
+        Person,
+        resolver=DefaultResolver(PersonModel),
+        filters=graphene.List(FilterSet, required=False),
+    )
+
 
 schema = graphene.Schema(query=Query)
