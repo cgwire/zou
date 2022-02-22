9b60ce04ab9973b8cbd54f48aa6b416aacb9761d
Evan Blaudy <evan@cg-wire.com>
2022-02-22 07:31:50 +0100

    [estimations] add a route to upload a csv for estimations relative to assets or shots tasks

---
+++ zou/app/blueprints/source/csv/task_type_estimations.py
@@ -0,0 +1,99 @@
+from slugify import slugify
+from zou.app.blueprints.source.csv.base import BaseCsvProjectImportResource
+from zou.app.models.task import Task
+from zou.app.models.organisation import Organisation
+
+from zou.app.services import assets_service, shots_service, tasks_service
+
+from sqlalchemy import and_
+
+from zou.app.utils import date_helpers
+
+
+class TaskTypeEstimationsCsvImportResource(BaseCsvProjectImportResource):
+    def prepare_import(self, project_id, task_type_id, episode_id=None):
+        self.organisation = Organisation.query.first()
+        self.assets_map = {}
+        self.shots_map = {}
+
+        asset_types_map = {}
+        asset_types = assets_service.get_asset_types_for_project(project_id)
+        for asset_type in asset_types:
+            asset_types_map[asset_type["id"]] = slugify(asset_type["name"])
+
+        criterions_assets = {"project_id": project_id}
+        if episode_id is not None and episode_id not in ["main", "all"]:
+            criterions_assets["source_id"] = episode_id
+        assets = assets_service.get_assets(criterions_assets)
+        for asset in assets:
+            key = "%s%s" % (
+                asset_types_map[asset["entity_type_id"]],
+                slugify(asset["name"]),
+            )
+            self.assets_map[key] = asset["id"]
+
+        sequences_map = {}
+        criterions_sequences = {"project_id": project_id}
+        if episode_id is not None and episode_id not in ["main", "all"]:
+            criterions_sequences["parent_id"] = episode_id
+        sequences = shots_service.get_sequences(criterions_sequences)
+        for sequence in sequences:
+            sequences_map[sequence["id"]] = slugify(sequence["name"])
+
+        shots = shots_service.get_shots({"project_id": project_id})
+        for shot in shots:
+            sequence_key = sequences_map.get(shot["parent_id"])
+            if sequence_key is not None:
+                key = "%s%s" % (sequence_key, slugify(shot["name"]))
+                self.shots_map[key] = shot["id"]
+
+    def import_row(self, row, project_id, task_type_id, episode_id=None):
+        key = slugify("%s%s" % (row["Parent"], row["Entity"]))
+
+        if self.assets_map.get(key):
+            entity_id = self.assets_map[key]
+        if self.shots_map.get(key):
+            entity_id = self.shots_map[key]
+
+        task_id = (
+            Task.query.filter(
+                and_(
+                    Task.project_id == project_id,
+                    Task.task_type_id == task_type_id,
+                    Task.entity_id == entity_id,
+                )
+            )
+            .one()
+            .id
+        )
+
+        new_data = {}
+
+        new_data["estimation"] = round(
+            float(row["Estimation"]) * self.organisation.hours_by_day * 60
+        )
+
+        try:
+            new_data["start_date"] = date_helpers.get_date_from_string(
+                row["Start date"]
+            )
+        except:
+            pass
+
+        try:
+            new_data["due_date"] = date_helpers.get_date_from_string(
+                row["Due date"]
+            )
+        except:
+            if new_data.get("start_date"):
+                new_data["due_date"] = date_helpers.add_business_days_to_date(
+                    new_data["start_date"], float(row["Estimation"]) - 1
+                )
+
+        tasks_service.update_task(task_id, new_data)
+
+
+class TaskTypeEstimationsEpisodeCsvImportResource(
+    TaskTypeEstimationsCsvImportResource
+):
+    pass
