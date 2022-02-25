afb9f222b59e616d2a64832c1c05df7869e25c29
Evan Blaudy <evan@cg-wire.com>
2022-02-25 02:20:44 +0100

    [projects][events] deleting a project now trigger an event project:delete

--- zou/app/blueprints/crud/project.py
+++ zou/app/blueprints/crud/project.py
@@ -11,7 +11,7 @@ from zou.app.services import (
     shots_service,
     user_service,
 )
-from zou.app.utils import permissions, fields
+from zou.app.utils import events, permissions, fields
 
 from .base import BaseModelResource, BaseModelsResource
 
@@ -100,6 +100,7 @@ class ProjectResource(BaseModelResource):
                 deletion_service.remove_project(instance_id)
             else:
                 project.delete()
+                events.emit("project:delete", {"project_id": project.id})
             self.post_delete(project_dict)
             return "", 204
 
