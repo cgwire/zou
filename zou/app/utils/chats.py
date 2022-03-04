4f3af960b2163119c664202f704ecc243211c977
Evan Blaudy <evan@cg-wire.com>
2022-03-04 11:49:09 +0100

    [notifications] add discord integration

--- zou/app/utils/chats.py
+++ zou/app/utils/chats.py
@@ -1,6 +1,12 @@
 from slackclient import SlackClient
 from matterhook import Webhook
+from discord import (
+    Client as DiscordClient,
+    Intents as DiscordIntents,
+    Embed as DiscordEmbed,
+)
 from zou.app import config
+import asyncio
 
 
 def send_to_slack(app_token, userid, message):
@@ -26,3 +32,41 @@ def send_to_mattermost(webhook, userid, message):
 
     # send a message to the API_KEY's channel
     mwh.send(message["message"], channel="@%s" % userid)
+
+
+def send_to_discord(current_app, token, userid, message):
+    async def send_to_discord_async(current_app, token, userid, message):
+        intents = DiscordIntents.default()
+        intents.members = True
+        client = DiscordClient(intents=intents)
+
+        @client.event
+        async def on_ready(
+            current_app=current_app, userid=userid, message=message
+        ):
+            user_found = False
+            for user in client.get_all_members():
+                if (
+                    "%s#%s" % (user.name, user.discriminator) == userid
+                    and not user.bot
+                ):
+                    embed = DiscordEmbed()
+                    embed.description = message
+                    await user.send(embed=embed)
+                    user_found = True
+                    break
+            if not user_found:
+                current_app.logger.info(
+                    "User %s not found by discord bot" % userid
+                )
+
+            await client.close()
+
+        await client.start(token)
+
+    asyncio.set_event_loop(asyncio.new_event_loop())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(
+        send_to_discord_async(current_app, token, userid, message)
+    )
+    loop.close()
