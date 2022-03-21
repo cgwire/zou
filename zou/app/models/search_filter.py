f141ef7e2c0bad0223f8d836eec8e3cfb44e372e
Evan Blaudy <evan@cg-wire.com>
2022-03-21 17:27:53 +0100

    [search filter] allow search filters to have more characters (500 max, it was 200)

--- zou/app/models/search_filter.py
+++ zou/app/models/search_filter.py
@@ -14,7 +14,7 @@ class SearchFilter(db.Model, BaseMixin, SerializerMixin):
     list_type = db.Column(db.String(80), nullable=False, index=True)
     entity_type = db.Column(db.String(80))
     name = db.Column(db.String(200), nullable=False, default="")
-    search_query = db.Column(db.String(200), nullable=False, default="")
+    search_query = db.Column(db.String(500), nullable=False, default="")
 
     person_id = db.Column(UUIDType(binary=False), db.ForeignKey("person.id"))
     project_id = db.Column(UUIDType(binary=False), db.ForeignKey("project.id"))
