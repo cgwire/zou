048e9b44352737f2a70392d0affcff5963a778b5
Frank Rousseau <frank.rousseau@free.fr>
2022-05-25 17:32:39 +0200

    [automations] Send proper message when an automation cant be deleted

--- zou/app/blueprints/crud/status_automation.py
+++ zou/app/blueprints/crud/status_automation.py
@@ -1,7 +1,11 @@
-from zou.app.models.status_automation import StatusAutomation
+from flask_jwt_extended import jwt_required
+from flask_restful import current_app
+from sqlalchemy.exc import IntegrityError, StatementError
 
 from .base import BaseModelsResource, BaseModelResource
 
+from zou.app.models.status_automation import StatusAutomation
+from zou.app.models.project import ProjectStatusAutomationLink
 from zou.app.services import status_automations_service, user_service
 
 
@@ -25,6 +29,37 @@ class StatusAutomationResource(BaseModelResource):
     def post_update(self, status_automation):
         status_automations_service.clear_status_automation_cache()
         return status_automation
+
+    @jwt_required
+    def delete(self, instance_id):
+        """
+        Delete status automation corresponding at given ID and return it as a JSON
+        object.
+        """
+        instance = self.get_model_or_404(instance_id)
+        instance_dict = instance.serialize()
+        links = ProjectStatusAutomationLink.query.filter_by(
+            status_automation_id=instance_dict["id"]
+        ).all()
+        if len(links) > 0:
+            return {"message": "This automation is used in a project."}, 400
+
+        try:
+            self.check_delete_permissions(instance_dict)
+            self.pre_delete(instance_dict)
+            instance.delete()
+            self.emit_delete_event(instance_dict)
+            self.post_delete(instance_dict)
+
+        except IntegrityError as exception:
+            current_app.logger.error(str(exception), exc_info=1)
+            return {"message": str(exception)}, 400
+
+        except StatementError as exception:
+            current_app.logger.error(str(exception), exc_info=1)
+            return {"message": str(exception)}, 400
+
+        return "", 204
 
     def post_delete(self, status_automation):
         status_automations_service.clear_status_automation_cache()
