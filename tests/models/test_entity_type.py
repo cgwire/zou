8828fe6693dc4b6eaa4a9fe37028360c9205d9db
Félix David <felixg.david@gmail.com>
2022-06-03 15:44:23 +0200

    fix tests

--- tests/models/test_entity_type.py
+++ tests/models/test_entity_type.py
@@ -1,6 +1,7 @@
 from tests.base import ApiDBTestCase
 
 from zou.app.models.entity_type import EntityType
+from zou.app.models.task_type import TaskType
 
 from zou.app.utils import fields
 
@@ -8,7 +9,7 @@ from zou.app.utils import fields
 class EntityTypeTestCase(ApiDBTestCase):
     def setUp(self):
         super(EntityTypeTestCase, self).setUp()
-        self.generate_data(EntityType, 3)
+        self.generate_fixture_asset_types()
 
     def test_get_entity_types(self):
         entity_types = self.get("data/entity-types")
@@ -30,6 +31,38 @@ class EntityTypeTestCase(ApiDBTestCase):
         entity_types = self.get("data/entity-types")
         self.assertEqual(len(entity_types), 4)
 
+    def test_create_asset_type_with_task_types(self):
+        self.generate_fixture_department()
+        self.department_id = self.department.id
+        self.generate_fixture_task_type()
+
+        task_types = [
+            str(task_type.id) for task_type in TaskType.query.all()
+        ]
+        task_types = [str(self.task_type_concept.id), str(self.task_type_modeling.id)]
+
+        data = {
+            "name": "FX",
+            "task_types": task_types
+        }
+        self.asset_type = self.post("data/entity-types", data)
+        self.assertIsNotNone(self.asset_type["id"])
+        self.assertEquals(
+            set(self.asset_type["task_types"]),
+            set(task_types),
+        )
+
+        asset_types = self.get("data/entity-types")
+        self.assertEqual(len(asset_types), 4)
+
+        created_asset_type = EntityType.get(self.asset_type["id"])
+        self.assertEquals(
+            set(
+                str(task_type.id) for task_type in created_asset_type.task_types
+            ),
+            set(task_types),
+        )
+
     def test_update_entity_types(self):
         entity_types = self.get_first("data/entity-types")
         data = {"name": "sequence"}
@@ -39,6 +72,26 @@ class EntityTypeTestCase(ApiDBTestCase):
         )
         self.assertEqual(data["name"], entity_types_again["name"])
         self.put_404("data/entity-types/%s" % fields.gen_uuid(), data)
+
+    def test_update_asset_type_with_task_types(self):
+        self.generate_fixture_department()
+        self.department_id = self.department.id
+        self.generate_fixture_task_type()
+
+        asset_type = self.get_first("data/entity-types")
+        task_types = [
+            str(task_type.id) for task_type in TaskType.query.all()
+        ]
+        data = {
+            "name": "FX",
+            "task_types": task_types
+        }
+        self.put("data/entity-types/%s" % asset_type["id"], data)
+        asset_type_again = self.get("data/entity-types/%s" % asset_type["id"])
+        self.assertEquals(
+            set(task_type for task_type in asset_type_again["task_types"]),
+            set(task_types),
+        )
 
     def test_delete_entity_types(self):
         entity_types = self.get("data/entity-types")
--- tests/models/test_task_type.py
+++ tests/models/test_task_type.py
@@ -1,7 +1,5 @@
 from tests.base import ApiDBTestCase
-from zou.app.models.entity_type import EntityType
 from zou.app.models.task_type import TaskType
-from zou.app.services.assets_service import build_entity_type_asset_type_filter
 
 from zou.app.utils import fields
 
@@ -11,11 +9,11 @@ class TaskTypeTestCase(ApiDBTestCase):
         super(TaskTypeTestCase, self).setUp()
         self.generate_fixture_department()
         self.department_id = self.department.id
-        self.generate_fixture_task_type()
+        self.generate_data(TaskType, 3, department_id=self.department_id)
 
     def test_get_task_types(self):
         task_types = self.get("data/task-types")
-        self.assertEqual(len(task_types), 6)
+        self.assertEqual(len(task_types), 3)
 
     def test_get_task_type(self):
         task_type = self.get_first("data/task-types")
@@ -31,41 +29,10 @@ class TaskTypeTestCase(ApiDBTestCase):
         }
         self.task_type = self.post("data/task-types", data)
         self.assertIsNotNone(self.task_type["id"])
-
-        # Test to not create twice the same, '400' error code
         self.task_type = self.post("data/task-types", data, 400)
 
         task_types = self.get("data/task-types")
-        self.assertEqual(len(task_types), 7)
-
-    def test_create_task_type_with_asset_types(self):
-        self.generate_fixture_asset_types()
-        asset_types = [
-            str(asset_type.id) for asset_type in EntityType.query.filter(build_entity_type_asset_type_filter())
-        ]
-        data = {
-            "name": "animation",
-            "color": "#000000",
-            "department_id": self.department_id,
-            "asset_types": asset_types
-        }
-        self.task_type = self.post("data/task-types", data)
-        self.assertIsNotNone(self.task_type["id"])
-        self.assertEquals(
-            set(self.task_type["asset_types"]),
-            set(asset_types),
-        )
-
-        task_types = self.get("data/task-types")
-        self.assertEqual(len(task_types), 7)
-
-        created_task_type = TaskType.get(self.task_type["id"])
-        self.assertEquals(
-            set(
-                str(task_type.id) for task_type in created_task_type.asset_types
-            ),
-            set(asset_types),
-        )
+        self.assertEqual(len(task_types), 4)
 
     def test_update_task_type(self):
         task_type = self.get_first("data/task-types")
@@ -75,28 +42,11 @@ class TaskTypeTestCase(ApiDBTestCase):
         self.assertEqual(data["color"], task_type_again["color"])
         self.put_404("data/task-types/%s" % fields.gen_uuid(), data)
 
-    def test_update_task_type_with_asset_types(self):
-        self.generate_fixture_asset_types()
-        task_type = self.get_first("data/task-types")
-        asset_types = [
-            str(asset_type.id) for asset_type in EntityType.query.filter(build_entity_type_asset_type_filter())
-        ]
-        data = {
-            "name": "layout",
-            "asset_types": asset_types
-        }
-        self.put("data/task-types/%s" % task_type["id"], data)
-        task_type_again = self.get("data/task-types/%s" % task_type["id"])
-        self.assertEquals(
-            set(asset_type for asset_type in task_type_again["asset_types"]),
-            set(asset_types),
-        )
-
     def test_delete_task_type(self):
         task_types = self.get("data/task-types")
-        self.assertEqual(len(task_types), 6)
+        self.assertEqual(len(task_types), 3)
         task_type = task_types[0]
         self.delete("data/task-types/%s" % task_type["id"])
         task_types = self.get("data/task-types")
-        self.assertEqual(len(task_types), 5)
+        self.assertEqual(len(task_types), 2)
         self.delete_404("data/task-types/%s" % fields.gen_uuid())
