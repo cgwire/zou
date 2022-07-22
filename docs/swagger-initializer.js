df4577e7fa2f3d6fead879da5f504ac157032f64
NehmatH <nehmat_h@hotmail.com>
2022-07-22 12:04:20 +0200

    Update openAPI file and adjust its location in the url

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: window.location.protocol + '//' + window.location.host + '/openapi.json',
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
@@ -16,5 +16,13 @@ window.onload = function() {
     layout: "StandaloneLayout"
   });
 
+  topbar = document.getElementsByClassName('topbar-wrapper')[0]
+  topbar.getElementsByTagName('img')[0].src = "kitsu.svg";
+  
+  var div = document.createElement("div");
+  div.setAttribute('class', 'kitsu-title');
+  div.innerHTML = "Kitsu API Documentation";
+
+  topbar.getElementsByClassName("link")[0].appendChild(div);
   //</editor-fold>
 };
