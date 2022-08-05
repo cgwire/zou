cfc4a6de68b79cf24d71ba628ac5aa8d50015b54
NehmatH <nehmat_h@hotmail.com>
2022-08-05 02:53:52 +0200

    Add filter by tags feature, modify its css and make it case insensitive

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -1,17 +1,29 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
-
+  const CaseInsensitiveFilterPlugin = function (system) {
+    return {
+        fn: {
+            opsFilter: (taggedOps, phrase) => {
+                return taggedOps.filter((tagObj, tag) => tag.toLowerCase().indexOf(phrase.toLowerCase()) !== -1);
+            }
+        }
+    }
+  };
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
+    
     url: window.location.protocol + '//' + window.location.host + '/openapi.json',
     dom_id: '#swagger-ui',
     deepLinking: true,
+    filter: true,
+    docExpansion: 'list',
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
     ],
     plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
+      SwaggerUIBundle.plugins.DownloadUrl,
+      CaseInsensitiveFilterPlugin
     ],
     layout: "StandaloneLayout"
   });
