298d13168126d4ec0a9c81dbfa72f72201e6aaa4
Frank Rousseau <frank.rousseau@free.fr>
2022-08-19 15:20:32 +0200

    [orgs] Handle properly non integer for nb hours per day field

--- zou/app/blueprints/crud/organisation.py
+++ zou/app/blueprints/crud/organisation.py
@@ -20,3 +20,8 @@ class OrganisationResource(BaseModelResource):
 
     def check_read_permissions(self, instance):
         return True
+
+    def pre_update(self, instance_dict, data):
+        if "hours_by_day" in data:
+            data["hours_by_day"] = int(float(data["hours_by_day"]))
+        return data
