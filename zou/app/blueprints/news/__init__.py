4665c7e285e694dd0ea673f84628ec95722c826b
Frank Rousseau <frank.rousseau@free.fr>
2023-02-23 18:51:25 +0100

    [qa] Add more filters to the newsfeed route
    * Allow to grab news for all open productions
    * Allow to filter news on episode id (only for shots)

--- zou/app/blueprints/news/__init__.py
+++ zou/app/blueprints/news/__init__.py
@@ -1,10 +1,15 @@
 from flask import Blueprint
 from zou.app.utils.api import configure_api_from_blueprint
 
-from .resources import ProjectNewsResource, ProjectSingleNewsResource
+from .resources import (
+    NewsResource,
+    ProjectNewsResource,
+    ProjectSingleNewsResource
+)
 
 
 routes = [
+    ("/data/projects/news", NewsResource),
     ("/data/projects/<project_id>/news", ProjectNewsResource),
     ("/data/projects/<project_id>/news/<news_id>", ProjectSingleNewsResource),
 ]
