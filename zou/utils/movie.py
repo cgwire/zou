3d8e7845bf57b64aed3a42ca2b294c1acf85130a
Fabian467 <fabiansantiago467@gmail.com>
2023-07-26 16:28:25 +0200

    Corrected storage of the tile

--- zou/utils/movie.py
+++ zou/utils/movie.py
@@ -126,7 +126,7 @@ def generate_tile(movie_path, movie_fps):
     file_source_name = os.path.basename(movie_path)
     file_target_name = f"{file_source_name[:-4]}_tile.png"
     file_target_path = os.path.join(folder_path, file_target_name)
-
+    
     probe = ffmpeg.probe(movie_path)
     duration_in_seconds = float(probe['streams'][0]['duration'])
     float_movie_fps = float(movie_fps)
@@ -136,10 +136,10 @@ def generate_tile(movie_path, movie_fps):
     try:
         (
             ffmpeg.input(movie_path)
-            .output(file_target_name, vf=f'fps=1,scale=720:480,tile=8x{rows}')
+            .output(file_target_path, vf=f'scale=720:480,tile=8x{rows}')
             .run(quiet=True)
-        )
-    except ffmpeg._run.Error as e:
+        )   
+    except subprocess.CalledProcessError as e:
         print(f"Error generating tile: {e}")
         raise e
     
