4a666c8425e9e201984abe272a61469d386a3f43
Evan Blaudy <evan@cg-wire.com>
2023-08-07 12:17:42 +0200

    [csv] when importing csv of entities (shots, assets, edits) be permissive with the case of tasks statuses

--- zou/app/blueprints/source/csv/assets.py
+++ zou/app/blueprints/source/csv/assets.py
@@ -61,7 +61,7 @@ class AssetsCsvImportResource(BaseCsvProjectImportResource):
             .filter(TaskType.for_entity == "Asset")
         )
         self.task_statuses = {
-            status["id"]: [status[n] for n in ("name", "short_name")]
+            status["id"]: [status[n].lower() for n in ("name", "short_name")]
             for status in tasks_service.get_task_statuses()
         }
         self.current_user_id = get_current_user()["id"]
@@ -80,7 +80,7 @@ class AssetsCsvImportResource(BaseCsvProjectImportResource):
             task_status_id = None
             if task_status_name not in [None, ""]:
                 for status_id, status_names in self.task_statuses.items():
-                    if task_status_name in status_names:
+                    if task_status_name.lower() in status_names:
                         task_status_id = status_id
                         break
                 if task_status_id is None:
