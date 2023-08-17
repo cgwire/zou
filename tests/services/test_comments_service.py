e0d2cacbf6e10ef08039c1c75716fb0d9ca42438
Evan Blaudy <evan@cg-wire.com>
2023-08-17 13:57:13 +0200

    [qa][tests] remove DeprecationWarning: Please use assertEqual instead.

--- tests/services/test_comments_service.py
+++ tests/services/test_comments_service.py
@@ -6,7 +6,7 @@ from zou.app.services import (
     entities_service,
     persons_service,
     tasks_service,
-    exception
+    exception,
 )
 
 
@@ -44,22 +44,19 @@ class CommentsServiceTestCase(ApiDBTestCase):
 
     def test_new_comment(self):
         self.comment = comments_service.new_comment(
-            self.task.id,
-            self.task_status.id,
-            self.user["id"],
-            "first comment"
+            self.task.id, self.task_status.id, self.user["id"], "first comment"
         )
         self.assertTrue(self.comment["id"] is not None)
         self.assertTrue(self.comment["created_at"] is not None)
-        self.assertEquals(self.comment["object_id"], str(self.task.id))
+        self.assertEqual(self.comment["object_id"], str(self.task.id))
         self.comment = comments_service.new_comment(
             self.task.id,
             self.task_status.id,
             self.user["id"],
             "second comment",
-            created_at="2024-01-23 10:00:00"
+            created_at="2024-01-23 10:00:00",
         )
-        self.assertEquals(self.comment["created_at"], "2024-01-23T10:00:00")
+        self.assertEqual(self.comment["created_at"], "2024-01-23T10:00:00")
         # TODO test attachment files
 
     def test_create_comment(self):
@@ -92,10 +89,7 @@ class CommentsServiceTestCase(ApiDBTestCase):
 
     def test_manage_status_change(self):
         self.comment = comments_service.new_comment(
-            self.task.id,
-            self.task_status.id,
-            self.user["id"],
-            "first comment"
+            self.task.id, self.task_status.id, self.user["id"], "first comment"
         )
         task_status = self.task_status.serialize()
         retake_status = self.generate_fixture_task_status_retake().serialize()
@@ -105,79 +99,79 @@ class CommentsServiceTestCase(ApiDBTestCase):
             task_status, task, comment
         )
         self.assertFalse(status_changed)
-        self.assertEquals(task["retake_count"], 0)
+        self.assertEqual(task["retake_count"], 0)
         (task, status_changed) = comments_service._manage_status_change(
             retake_status, task, comment
         )
         self.assertTrue(status_changed)
-        self.assertEquals(task["retake_count"], 1)
+        self.assertEqual(task["retake_count"], 1)
         (task, status_changed) = comments_service._manage_status_change(
             self.wfa_status, task, comment
         )
         self.assertTrue(task["end_date"] is not None)
 
     def test_manage_subscriptions(self):
-        #TODO
+        # TODO
         pass
 
     def test_run_status_automation(self):
-        #TODO
+        # TODO
         pass
 
     def add_attachments_to_comment(self):
-        #TODO
+        # TODO
         pass
 
     def test_create_attachment(self):
-        #TODO
+        # TODO
         pass
 
     def test_get_attachment_file_path(self):
-        #TODO
+        # TODO
         pass
 
     def test_get_all_attachment_files_for_project(self):
-        #TODO
+        # TODO
         pass
 
     def test_manage_subscriptions(self):
-        #TODO
+        # TODO
         pass
 
     def test_run_status_automation(self):
-        #TODO
+        # TODO
         pass
 
     def add_attachments_to_comment(self):
-        #TODO
+        # TODO
         pass
 
     def test_create_attachment(self):
-        #TODO
+        # TODO
         pass
 
     def test_get_attachment_file_path(self):
-        #TODO
+        # TODO
         pass
 
     def test_get_all_attachment_files_for_project(self):
-        #TODO
+        # TODO
         pass
 
     def test_get_all_attachment_files_for_task(self):
-        #TODO
+        # TODO
         pass
 
     def test_acknowledge_comment(self):
-        #TODO
+        # TODO
         pass
 
     def test_ack_comment(self):
-        #TODO
+        # TODO
         pass
 
     def test_unack_comment(self):
-        #TODO
+        # TODO
         pass
 
     def test_reply_comment(self):
@@ -186,39 +180,37 @@ class CommentsServiceTestCase(ApiDBTestCase):
             self.task.id,
             self.task_status.id,
             self.user["id"],
-            "comment that starts a thread discussion"
+            "comment that starts a thread discussion",
         )
         comments_service.reply_comment(
-            comment["id"],
-            reply_text,
-            person_id=self.user["id"]
+            comment["id"], reply_text, person_id=self.user["id"]
         )
         comment = tasks_service.get_comment(comment["id"])
-        self.assertEquals(len(comment["replies"]), 1)
-        self.assertEquals(comment["replies"][0]["text"], reply_text)
-        self.assertEquals(comment["replies"][0]["person_id"], self.user["id"])
+        self.assertEqual(len(comment["replies"]), 1)
+        self.assertEqual(comment["replies"][0]["text"], reply_text)
+        self.assertEqual(comment["replies"][0]["person_id"], self.user["id"])
         comments_service.reply_comment(
             comment["id"],
             "mention @Animation and @John Doe",
-            person_id=self.user["id"]
+            person_id=self.user["id"],
         )
         tasks_service.clear_comment_cache(comment["id"])
         comment = tasks_service.get_comment(comment["id"])
-        self.assertEquals(len(comment["replies"]), 2)
+        self.assertEqual(len(comment["replies"]), 2)
         self.assertListEqual(
             comment["replies"][1]["mentions"], [self.person_id]
         )
         self.assertListEqual(
             comment["replies"][1]["department_mentions"],
-            [str(self.department_animation.id)]
+            [str(self.department_animation.id)],
         )
 
     def get_reply(self):
-        #TODO
+        # TODO
         pass
 
     def delete_reply(self):
-        #TODO
+        # TODO
         pass
 
     def test_reset_mentions(self):
@@ -226,13 +218,12 @@ class CommentsServiceTestCase(ApiDBTestCase):
             self.task.id,
             self.task_status.id,
             self.user["id"],
-            "mentions @Animation @John Doe"
+            "mentions @Animation @John Doe",
         )
         comment = comments_service.reset_mentions(self.comment)
         self.assertListEqual(comment["mentions"], [self.person_id])
         self.assertListEqual(
-            comment["department_mentions"],
-            [str(self.department_animation.id)]
+            comment["department_mentions"], [str(self.department_animation.id)]
         )
 
     def test_get_comment_mentions(self):
