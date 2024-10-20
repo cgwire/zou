94915d458a6596e29b9813e81cdc5f16b5e30833
Evan Blaudy <evan@cg-wire.com>
2024-10-21 01:48:08 +0200

    [config] for MAIL_DEBUG convert to bool

--- zou/app/utils/string.py
+++ zou/app/utils/string.py
@@ -6,6 +6,8 @@ def strtobool(val):
     """
     if isinstance(val, bool):
         return val
+    if isinstance(val, int):
+        return bool(val)
     elif val.lower() in ("y", "yes", "t", "true", "on", "1"):
         return True
     elif val.lower() in ("n", "no", "f", "false", "off", "0"):
