8982ee6086bdb50d9950de1d577491ff8e7a60c0
Evan Blaudy <evan@cg-wire.com>
2024-11-20 18:56:32 +0100

    [github] use main branch instead of master

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,8 @@
 set -e
-git pull --rebase origin master
+git pull --rebase origin main
 last_release_number=$(python -c "from zou import __version__; print(__version__)")
 release_number=$(echo ${last_release_number} | awk -F. -v OFS=. '{$NF += 1 ; print}')
 echo "__version__ = \"$release_number\"" > zou/__init__.py
 git commit zou/__init__.py -m $release_number
 git tag v$release_number
-git push origin master --tag
+git push origin main --tag
