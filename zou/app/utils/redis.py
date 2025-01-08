4c467fef28c733e54e5068dce48920945361436d
Evan Blaudy <evan@cg-wire.com>
2025-01-08 12:15:13 +0100

    [redis] fix redis url

--- zou/app/utils/redis.py
+++ zou/app/utils/redis.py
@@ -5,7 +5,8 @@ def get_redis_url():
     redis_host = config.KEY_VALUE_STORE["host"]
     redis_port = config.KEY_VALUE_STORE["port"]
     db_index = config.KV_EVENTS_DB_INDEX
-    redis_password = config.KEY_VALUE_STORE["password"] or ""
-    if redis_password:
-        redis_password += "@"
+    if config.KEY_VALUE_STORE["password"]:
+        redis_password = f":{config.KEY_VALUE_STORE['password']}@"
+    else:
+        redis_password = ""
     return f"redis://{redis_password}{redis_host}:{redis_port}/{db_index}"
