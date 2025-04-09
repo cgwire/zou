95568d7937cd6993ea545db06fe254a9fa9c57b9
Frank Rousseau <frank@cg-wire.com>
2025-04-09 10:26:32 +0200

    [rooms] Add more information to room state
    * User ID
    * Entity ID
    * Preview index

--- zou/event_stream.py
+++ zou/event_stream.py
@@ -25,10 +25,13 @@ socketio.init_app(app, message_queue=redis_url, async_mode="gevent")
 
 def _get_empty_room(current_frame=0):
     return {
+        "user_id": None,
         "people": [],
         "is_playing": False,
+        "current_entity_id": None,
         "current_entity_index": None,
         "current_preview_file_id": None,
+        "current_preview_file_index": None,
         "current_frame": current_frame,
         "is_repeating": None,
         "is_laser_mode": None,
@@ -61,14 +64,16 @@ def _leave_room(room_id, user_id):
 
 
 def _update_room_playing_status(data, room):
+    room["user_id"] = data.get("user_id", False)
     room["is_playing"] = data.get("is_playing", False)
     room["is_repeating"] = data.get("is_repeating", False)
     room["is_laser_mode"] = data.get("is_laser_mode", False)
-    room["current_entity_index"] = data["current_entity_index"]
+    room["current_entity_id"] = data.get("current_entity_id", None)
+    room["current_entity_index"] = data.get("current_entity_index", None)
+    room["current_preview_file_id"] = data.get("current_preview_file_id", None)
+    room["current_preview_file_index"] = data.get("current_preview_file_index", None)
     room["handle_in"] = data.get("handle_in", None)
     room["handle_out"] = data.get("handle_out", None)
-    if "current_preview_file_id" in data:
-        room["current_preview_file_id"] = data["current_preview_file_id"]
     if "current_frame" in data:
         room["current_frame"] = data["current_frame"]
     if "comparing" in data:
