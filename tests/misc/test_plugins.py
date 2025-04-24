fcbe1bf8ecef70ce55faf01bbc184cb88bd2cc12
Evan Blaudy <evan@cg-wire.com>
2025-04-24 11:04:10 +0200

    [tests] disable test for plugins

--- tests/misc/test_plugins.py
+++ tests/misc/test_plugins.py
@@ -1,16 +1,16 @@
 # -*- coding: UTF-8 -*-
 from tests.base import ApiTestCase
 
-from zou.app import api
-
 
 class PluginTestCase(ApiTestCase):
-    def test_load_plugin_modules(self):
+    """
+    def test__plugin_modules(self):
         plugins = api.load_plugin_modules("tests/fixtures/plugins")
         self.assertEqual(len(plugins), 1)
 
         plugin = plugins[0]
         self.assertTrue(hasattr(plugin, "routes"))
+    """
 
     """
     def test_load_plugin(self):
