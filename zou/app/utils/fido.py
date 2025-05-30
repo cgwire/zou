cee210a8423030ff53c45a15d8caf273d98d50a3
Evan Blaudy <evan@cg-wire.com>
2025-05-30 16:37:50 +0200

    [requirements] upgrade outdated requirements + drop python 3.9

--- zou/app/utils/fido.py
+++ zou/app/utils/fido.py
@@ -1,5 +1,3 @@
-import fido2.features
-
 from fido2.webauthn import (
     PublicKeyCredentialRpEntity,
 )
@@ -7,8 +5,6 @@ from fido2.server import Fido2Server
 from urllib.parse import urlparse
 
 from zou.app import config
-
-fido2.features.webauthn_json_mapping.enabled = True
 
 
 def get_fido_server():
