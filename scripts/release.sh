2c316321f06d7d51340f05ea7f90581f48af1a08
Evan Blaudy <evan@cg-wire.com>
2025-06-17 11:05:30 +0200

    [release] fix tags not pushed

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,5 +4,5 @@ last_release_number=$(python -c "from zou import __version__; print(__version__)
 release_number=$(echo ${last_release_number} | awk -F. -v OFS=. '{$NF += 1 ; print}')
 echo "__version__ = \"$release_number\"" > zou/__init__.py
 git commit zou/__init__.py -m $release_number
-git tag v$release_number
+git tag -a -m "Release v$release_number" v$release_number
 git push origin main --follow-tags
