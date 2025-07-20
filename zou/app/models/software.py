d39447b3a4e18989e08091f6a72623bf84912340
Frank Rousseau <frank@cg-wire.com>
2025-07-20 10:38:49 +0200

    [software] Add more information to the software model

--- zou/app/models/software.py
+++ zou/app/models/software.py
@@ -7,10 +7,14 @@ from sqlalchemy.dialects.postgresql import JSONB
 
 class Software(db.Model, BaseMixin, SerializerMixin):
     """
-    Describes software used by working files.
+    Describes software used in the studio.
     """
 
     name = db.Column(db.String(40), unique=True, nullable=False)
     short_name = db.Column(db.String(20), nullable=False)
+    archived = db.Column(db.Boolean, default=False)
+    version = db.Column(db.String(20), nullable=False)
     file_extension = db.Column(db.String(20), nullable=False)
     secondary_extensions = db.Column(JSONB)
+    monthly_cost = db.Column(db.Integer, default=0)
+    inventory_amount = db.Column(db.Integer, default=0)
