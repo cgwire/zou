24f2e1ec783bcc440421abe2cdcbab367934e00f
Nicolas Ledez <247138+nledez@users.noreply.github.com>
2026-02-10 20:56:27 +0100

    [tests] Cache: isolate rather than empty

--- zou/app/utils/cache.py
+++ zou/app/utils/cache.py
@@ -9,32 +9,31 @@ import redis
 from flask_caching import Cache
 from zou.app import config
 
-
 cache = None
 
-try:
-    redis_cache = redis.StrictRedis(
-        host=config.KEY_VALUE_STORE["host"],
-        port=config.KEY_VALUE_STORE["port"],
-        db=config.MEMOIZE_DB_INDEX,
-        password=config.KEY_VALUE_STORE["password"],
-        decode_responses=True,
-    )
-    redis_cache.get("test")
-    cache = Cache(
-        config={
-            "CACHE_TYPE": "redis",
-            "CACHE_REDIS_HOST": config.KEY_VALUE_STORE["host"],
-            "CACHE_REDIS_PORT": config.KEY_VALUE_STORE["port"],
-            "CACHE_REDIS_DB": config.MEMOIZE_DB_INDEX,
-            "CACHE_REDIS_PASSWORD": config.KEY_VALUE_STORE["password"],
-        }
-    )
-
-# This is needed to run tests which. This way they do not require a Redis
-# instance to work properly
-except redis.ConnectionError:
-    cache = Cache(config={"CACHE_TYPE": "simple"})
+if config.CACHE_TYPE is not None:
+    cache = Cache(config={"CACHE_TYPE": config.CACHE_TYPE})
+else:
+    try:
+        redis_cache = redis.StrictRedis(
+            host=config.KEY_VALUE_STORE["host"],
+            port=config.KEY_VALUE_STORE["port"],
+            db=config.MEMOIZE_DB_INDEX,
+            password=config.KEY_VALUE_STORE["password"],
+            decode_responses=True,
+        )
+        redis_cache.get("test")
+        cache = Cache(
+            config={
+                "CACHE_TYPE": "redis",
+                "CACHE_REDIS_HOST": config.KEY_VALUE_STORE["host"],
+                "CACHE_REDIS_PORT": config.KEY_VALUE_STORE["port"],
+                "CACHE_REDIS_DB": config.MEMOIZE_DB_INDEX,
+                "CACHE_REDIS_PASSWORD": config.KEY_VALUE_STORE["password"],
+            }
+        )
+    except redis.ConnectionError:
+        cache = Cache(config={"CACHE_TYPE": "simple"})
 
 memoize_function = cache.memoize
 
