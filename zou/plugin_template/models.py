41f855909db786c0c4434ae26714d577e511ccd8
Frank Rousseau <frank@cg-wire.com>
2026-02-27 11:18:47 +0100

    [plugins] Switch plugin template back to classic db.Model

--- zou/plugin_template/models.py
+++ zou/plugin_template/models.py
@@ -1,20 +1,13 @@
+from zou.app import db
 from zou.app.models.serializer import SerializerMixin
 from zou.app.models.base import BaseMixin
 
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import MetaData, Column, Integer
 
-from zou.app.utils.plugins import create_plugin_metadata
-
-plugin_metadata = create_plugin_metadata("myplugin")  # plugin id expected here
-PluginBase = declarative_base(metadata=plugin_metadata)
-
-
-class Count(PluginBase, BaseMixin, SerializerMixin):
+class Count(db.Model, BaseMixin, SerializerMixin):
     """
     A simple model to keep track of a count.
     """
 
-    __tablename__ = "hello_count"
+    __tablename__ = "plugin_myplugin_count"
     __table_args__ = {"extend_existing": True}
-    count = Column(Integer, nullable=False, default=0)
+    count = db.Column(db.Integer, nullable=False, default=0)
