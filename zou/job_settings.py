afda8c3f999330f6c02611a6ec61e74cc7b009b4
Nicolas Ledez <247138+nledez@users.noreply.github.com>
2026-03-20 10:35:06 +0100

    fix: call sync_config at RQ worker startup to sync Redis config values

--- zou/job_settings.py
+++ zou/job_settings.py
@@ -1,6 +1,9 @@
 from zou.app import config
+from zou.app.stores.config_store import sync_config
 
 REDIS_HOST = config.KEY_VALUE_STORE["host"]
 REDIS_PORT = config.KEY_VALUE_STORE["port"]
 REDIS_DB = config.KV_JOB_DB_INDEX
 REDIS_PASSWORD = config.KEY_VALUE_STORE["password"]
+
+sync_config()
